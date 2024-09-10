9209b7bcc9a893b411a30ae07d32673f414f49da
varasampath <varasampath753@gmail.com>
2024-09-10 15:19:13 +0530

    add login next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import AddPatientDetails from "@/components/custom/add-patient-details";
-import LoginBtn from "@/components/custom/login-btn";
 
 export default function Home() {
   return (
     <div className="flex flex-col  h-screen">
-      <LoginBtn />
       <AddPatientDetails />
     </div>
   );
